70e4fad610a85395392ac656c941d32f8a096a7e
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-07 02:10:45 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,7 +7,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.integer :price
       t.boolean :family_friendly
       t.date :opening_date
-      t.date :opening_date
+      t.date :closing_date
       # has a long, long desription
     end
   end
