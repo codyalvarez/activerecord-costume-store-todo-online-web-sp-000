cdd382f53fbc125845a55ff9fbe9a4cba0ea276e
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-07 01:49:51 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.string :costume_inventory
-      t.integer :employees_count
+      t.integer :num_of_employees
       knows if its still in business or permanently closed
       # has an opening time
       # has a closing time
